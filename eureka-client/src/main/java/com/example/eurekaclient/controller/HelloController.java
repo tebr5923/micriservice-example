99441f3f320499dfa3427a92d702a877d12eacd9
Dmitry Tebryaev <tebr5923@mail.ru>
2023-03-10 18:05:52 +0700

    add eureka.instance.instance-id in HelloController

--- eureka-client/src/main/java/com/example/eurekaclient/controller/HelloController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class HelloController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/hello")
     public String getHelloWorld() {
-        return "Hello World!!!" ;
+        return "Hello World!!! id= " + id ;
     }
 //
 //    @GetMapping("/hello/{name}")
