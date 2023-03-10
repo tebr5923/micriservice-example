cda85b3fb0629ae82655b97759ff3a9f428e2d98
Dmitry Tebryaev <tebr5923@mail.ru>
2023-03-10 17:10:50 +0700

    add api-gateway

--- eureka-client/src/main/java/com/example/eurekaclient/controller/HelloController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/HelloController.java
@@ -2,12 +2,23 @@ package com.example.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class HelloController {
-    @GetMapping("/hello/{name}")
-    public String getHelloWorld(@PathVariable String name) {
-        return "Hello World " + name;
+
+    @GetMapping("/hello")
+    public String getHelloWorld() {
+        return "Hello World!!!" ;
     }
+//
+//    @GetMapping("/hello/{name}")
+//    public String getHelloWorld(@PathVariable String name) {
+//        if (name == null) {
+//            name = "user";
+//        }
+//        return "Hello World " + name;
+//    }
 }
